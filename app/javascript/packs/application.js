3eaa13cccf037be3d24e4c99cb5b9813f4c71e70
Jorge Luis <jluis.developer@gmail.com>
2019-07-15 23:33:15 -0500

    Funcionando hasta el momento

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,27 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
+//
+// import TurbolinksAdapter from 'vue-turbolinks'
+// import Vue from 'vue/dist/vue.esm'
+// import App from '../app.vue'
+//
+// Vue.use(TurbolinksAdapter);
+//
+// document.addEventListener('turbolinks:load', () => {
+//     const app = new Vue({
+//         el: '[data-behavior="vue"]',
+//         data: () => {
+//             return {
+//                 message: "Can you say hello?"
+//             }
+//         },
+//         components: { App }
+//     })
+// });
